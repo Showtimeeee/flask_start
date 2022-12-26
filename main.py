d2d6c2bbd907581ab6d8bf1e4c3466e5c4ea7d86
Showtimeeeeexitgit config --global user.email sipovichit@yandex.rugit config --global user.name Showtimeeee <sipovichit@yandex.ru>
2022-12-26 17:39:05 +0300

    pages structure

--- main.py
+++ main.py
@@ -2,10 +2,40 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+lst_main_menu = [{'name': 'Главная', 'url': '/'},
+                 {'name': 'Добавить', 'url': '/add'},
+                 {'name': 'Обновить', 'url': '/update'},
+                 {'name': 'Удалить', 'url': '/delete'},
+                 {'name': 'Просмотр', 'url': '/show'}]
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', name='Seva')
+    return render_template('index.html', name='Seva', title='Главная страница', lst_main_menu=lst_main_menu)
+
+
+# @app.route('/about')
+# def about():
+#     return render_template('about.html', name='Seva', title='О проекте', lst_main_menu=lst_main_menu)
+
+
+@app.route('/add')
+def add():
+    return render_template('add.html', title='Добавить', lst_main_menu=lst_main_menu)
+
+@app.route('/update')
+def update():
+    return render_template('update.html', title='Обновить', lst_main_menu=lst_main_menu)
+
+
+@app.route('/delete')
+def delete():
+    return render_template('delete.html', title='Удалить', lst_main_menu=lst_main_menu)
+
+
+@app.route('/show')
+def show():
+    return render_template('show.html', title='Просмотр', lst_main_menu=lst_main_menu)
 
 
 if __name__ == "__main__":
